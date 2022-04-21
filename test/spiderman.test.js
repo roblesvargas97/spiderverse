6254296b8249c2cf310acb365da8daf2b27c3399
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-20 23:12:03 -0500

    Add Test

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -4,3 +4,20 @@ describe("Test Suite Dummy Description", () => {
     expect(resultOfSomething).toBe(3);
   });
 });
+
+describe("Unit test for spiderman class", () => {
+  test("Create an spiderman Object ", () => {
+    const AndrewGarfield = new Spiderman(
+      "Spiderman Sony",
+      31,
+      "Andrew Garfield",
+      2,
+      "Sony"
+    );
+    expect(AndrewGarfield.name).toBe("Spiderman Sony");
+    expect(AndrewGarfield.age).toBe(31);
+    expect(AndrewGarfield.actor).toBe("Andrew Garfield");
+    expect(AndrewGarfield.movies).toBe(2);
+    expect(AndrewGarfield.studio).toBe("Sony");
+  });
+});
